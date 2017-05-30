ae4ffbfcbfb4967298f135aa66505d3aac2918cc
YeahThatGuy <cr.josan@gmail.com>
2017-05-30 15:27:52 +0100

    Update and rename Bank113.java to R113.java

--- 01-Introduction/R113.java
+++ 01-Introduction/R113.java
@@ -1,16 +1,16 @@
-public class Bank113 {
+public class R113 {
 
     public static void main(String[] args) {
 
         double balance = 10000;
-        double interestRate = 0.5;
+        double interestRate = 0.005;
         double withdrawal = 500;
 
         int timeMonths = 0;
 
         while (balance > 0) {
-            balance = ((balance * 0.005) + balance) - 500;
-
+            balance = ((balance * interestRate) + balance) - 500;
+            //this control statement is needed for the last loop, when balance < 0
             if (balance > 0) {
                 timeMonths++;
                 System.out.println(balance);
