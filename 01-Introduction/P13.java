99559e1890d57889886392e7299657a497753923
YeahThatGuy <cr.josan@gmail.com>
2017-06-03 14:51:24 +0100

    Added sout comparison with unformatted Math.PI

--- 01-Introduction/P13.java
+++ 01-Introduction/P13.java
@@ -2,20 +2,18 @@ package ch1;
 
 import java.lang.Math.*;
 import java.text.DecimalFormat;
-import java.util.Scanner;
 
 public class P13 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of PI digits: ");
-        int piDigits = sc.nextInt();
+       int piDigits = 6;
 
         double cut = Math.PI / Math.pow(10, -piDigits);
         double result = cut * Math.pow(10, -piDigits);
         result = Double.parseDouble(new DecimalFormat("##.######").format(result));
         
-        System.out.println(result);
+        System.out.println("Unformatted PI: " + Math.PI);
+        System.out.println("PI rounded to the first 6 decimal places: " + result);
     }
 }
