074d8f9692016335434d24ac117aac4424320bb8
YeahThatGuy <cr.josan@gmail.com>
2017-06-03 14:42:02 +0100

    Updated R115

--- 01-Introduction/R115.java
+++ 01-Introduction/R115.java
@@ -1,56 +1,66 @@
+package housepaint;
+
 /* R1.15 
-Scanner (width, length, height, noDoors, noWindows, dwWidth, dwLength, dwHeight)
-Loop:
-	for doors (calculate total surface area)
-	for windows (calculate total surface area)
-Calculate house total surface area
-Subtract doors+windows total surface area from house total surface area
+ Scanner (width, length, height, noDoors, noWindows, dwWidth, dwLength, dwHeight)
+ Loop:
+ for doors (calculate total surface area)
+ for windows (calculate total surface area)
+ Calculate house total surface area
+ Subtract doors+windows total surface area from house total surface area
 
-Assumes that all objects are rectangular prisms.
+ Assumes that all objects are rectangular prisms.
 
-http://www.math.com/tables/geometry/surfareas.htm */
-
+ http://www.math.com/tables/geometry/surfareas.htm */
 import java.util.Scanner;
 
-public class R115 {
+public class HousePaint {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("House length: ");
+        System.out.print("House length: ");
         double length = sc.nextDouble();
-        System.out.println("House width: ");
+        System.out.print("House width: ");
         double width = sc.nextDouble();
-        System.out.println("House height: ");
+        System.out.print("House height: ");
         double height = sc.nextDouble();
-        System.out.println("Number of doors: ");
+        System.out.print("Number of doors: ");
         double noDoors = sc.nextDouble();
-        System.out.println("Door length: ");
+        System.out.print("Door length: ");
         double dLength = sc.nextDouble();
-        System.out.println("Door width: ");
+        System.out.print("Door width: ");
         double dWidth = sc.nextDouble();
-        System.out.println("Door height: ");
+        System.out.print("Door height: ");
         double dHeight = sc.nextDouble();
-        System.out.println("Number of windows: ");
+        System.out.print("Number of windows: ");
         double noWindows = sc.nextDouble();
-        System.out.println("Window length: ");
+        System.out.print("Window length: ");
         double wLength = sc.nextDouble();
-        System.out.println("Window width: ");
+        System.out.print("Window width: ");
         double wWidth = sc.nextDouble();
-        System.out.println("Window height: ");
+        System.out.print("Window height: ");
         double wHeight = sc.nextDouble();
         sc.close();
-        
+
         double doorsTotal = 0;
         double windowsTotal = 0;
-        double houseArea = 0; 
+        double houseArea = 0;
         double housePaint = 0; //surface area sans doors and windows
-        
+
         for (double cuntD = 0; cuntD < noDoors; cuntD++) {
             doorsTotal = ((2 * dWidth * dHeight) + (2 * dHeight * dLength) + (2 * dWidth * dLength)) + doorsTotal;
             System.out.println(doorsTotal);
         }
+
+        for (double cuntW = 0; cuntW < noWindows; cuntW++) {
+            windowsTotal = ((2 * wWidth * wHeight) + (2 * wHeight * wLength) + (2 * wWidth * wLength)) + windowsTotal;
+            System.out.println(windowsTotal);
+        }
         
+        houseArea = (2 * width * height) + (2 * height * length) + (2 * width * length);
+        housePaint = houseArea - (doorsTotal + windowsTotal);
+        System.out.println("Total house are: " + houseArea);
+        System.out.println("House area to be painted: " + housePaint + " m^2");
     }
 
 }
