ef7424f3b692bb4e4694c816853119b4f4b65952
YeahThatGuy <YeahThatGuy@github.com>
2017-08-03 20:49:43 +0100

    Added several exercises on loops.

---
+++ 06-Loops/Chapter6Review/SelfCheck6.java
@@ -0,0 +1,23 @@
+package Chapter6Review;
+
+public class SelfCheck6 {
+
+    public static void main(String[] args) {
+
+        /*
+        int n = 5;
+        while (n >= 0) {
+            n--;
+            System.out.println(n);
+        }
+         */
+        
+        int n = 1;
+        while (n <= 3) {
+            System.out.print(n + ", ");
+            n++;
+        }
+        
+    }
+
+}
